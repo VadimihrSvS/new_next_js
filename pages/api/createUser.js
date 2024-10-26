d89fec0eb14671a9cc09631ee59489d5c91da81a
VadimirSvS <vadimirsvs@gmail.com>
2024-10-27 01:03:22 +0300

    fix

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,7 +1,7 @@
 import prisma from '../../lib/prisma'
 
 export default async function handler(req, res) {
-
+    console.log(req.method)
     if (req.method === 'POST') {
         const { name } = req.body;
         const result = await prisma.user.create({
@@ -11,6 +11,8 @@ export default async function handler(req, res) {
             },
         });
         res.status(201).json(result);
+    } else if (req.method === 'GET') {
+        return res.json({ message: "Hello world" })
     } else {
         res.status(405).json({ message: 'Method not allowed' });
     }
