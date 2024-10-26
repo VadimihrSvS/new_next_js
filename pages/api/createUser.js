8442b7b7bbc680d034969ac2340673ee0306e3ea
VadimirSvS <vadimirsvs@gmail.com>
2024-10-27 01:57:20 +0300

    fixfix

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,17 +1,16 @@
 import prisma from '../../lib/prisma'
 
 export default async function handler(req, res) {
-    console.log(req.method)
     if (req.method === 'POST') {
-        // const { name } = req.body;
-        // const result = await prisma.user.create({
-        //     data: {
-        //         name,
-        //         role: "noname"
-        //     },
-        // });
-        console.log("Должно работать")
-        res.status(201).json({ name: "success" });
+        const { name } = req.body;
+        const result = await prisma.user.create({
+            data: {
+                role: "noname",
+                firstName: name,
+                lastName: "lastName",
+            },
+        });
+        res.status(201).json(result);
     } else if (req.method === 'GET') {
         return res.json({ message: "Hello world" })
     } else {
